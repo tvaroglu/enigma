b6ba044923459b223bf7636826f05aea1ef3ec65
Taylor Varoglu <tava8398@colorado.edu>
2021-06-11 10:28:25 -0600

    Complete KeyGen instance methods

--- spec/key_gen_spec.rb
+++ spec/key_gen_spec.rb
@@ -6,46 +6,60 @@ RSpec.describe KeyGen do
     key = KeyGen.new
 
     expect(key.class).to eq(KeyGen)
-    expect(key.key.class).to eq(String)
-    expect(key.key.length).to eq(5)
+
+    expect(key.reveal.class).to eq(Array)
+    expect(key.reveal.length).to eq(2)
+    expect(key.reveal.first.class).to eq(String)
+    expect(key.reveal.first.length).to eq(5)
+    expect(key.reveal.last).to eq('')
   end
 
-  it 'can generate random integer sequences' do
+  it 'can generate random integer keys' do
     key1 = KeyGen.new
     key2 = KeyGen.new
 
-    expect(key1.key).not_to eq(key2.key)
+    expect(key1.reveal[0]).not_to eq(key2.reveal[0])
   end
 
   it 'can return date offsets based on date of message transmission' do
     key = KeyGen.new
-    allow(Date).to receive(:today).and_return('2021-06-10')
+    allow(Date).to receive(:today).and_return('2021-06-11')
     today = Date.today
     another_day = '040895'
 
-    expected = key.reduce_date(today)
-    expect(expected).to eq('5641')
+    a_string = 'hello!'
+    invalid_date = '000000'
+    another_invalid_date = 1231231231231
 
-    expected = key.reduce_date
-    expect(expected).to eq('5641')
+    expected_default_return_values = [
+      key.return_offsets(today),
+      key.return_offsets,
+      key.return_offsets(a_string),
+      key.return_offsets(invalid_date),
+      key.return_offsets(another_invalid_date)
+    ]
+    expect(expected_default_return_values.all? { |result| result == '5641' }).to be true
 
-    expected = key.reduce_date(another_day)
-    expect(expected).to eq('1025')
+    expected_custom_return_value = key.return_offsets(another_day)
+    expect(expected_custom_return_value).to eq('1025')
   end
 
   it 'can return total shifts based on key and date offsets' do
-    mock_key_gen = instance_double('mock_key_gen', key: '02715')
-    allow(mock_key_gen).to receive(:reduce_date).and_return('1025')
-    allow(mock_key_gen).to receive(:return_offsets).and_call_original
+    key = KeyGen.new
+    key.instance_variable_set(:@key, '02715')
+    key.instance_variable_set(:@offsets, '1025')
 
-    expected = mock_key_gen.return_offsets
-    # require "pry"; binding.pry
+    expect(key.reveal[0]).to eq('02715')
+    expect(key.reveal[1]).to eq('1025')
+
+    expected = key.return_shifts
+
     expect(expected.class).to eq(Hash)
     expect(expected.keys.length).to eq(4)
-    expect(expected['A']).to eq(3)
-    expect(expected['B']).to eq(27)
-    expect(expected['C']).to eq(73)
-    expect(expected['D']).to eq(20)
+    expect(expected[:a]).to eq(3)
+    expect(expected[:b]).to eq(27)
+    expect(expected[:c]).to eq(73)
+    expect(expected[:d]).to eq(20)
   end
 
 end
